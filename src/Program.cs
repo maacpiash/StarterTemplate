2af219901bfdf8a02f76c257a2cfc99976c9098c
Ahad Chowdhury <ahad@maacpiash.com>
2025-05-25 15:21:21 +1000

    ➕ Add and initialise package

--- src/Program.cs
+++ src/Program.cs
@@ -26,6 +26,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
+builder.Services.AddJSComponents();
 
 var app = builder.Build();
 
